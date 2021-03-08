4124479c2835b2c67cd7a90517638b77f77b5674
prexadedia <34688050+prexadedia@users.noreply.github.com>
2021-03-08 00:12:21 -0500

    Add files via upload

---
+++ ml_assignment1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1.  FAANG stock dataset:
+# 
+# - Link: https://www.kaggle.com/aayushmishra1512/faang-complete-stock-data
+# - Purpose : I can use this dataset to understand the stocks in depth and to find the max highest price of the stocks and raise and downfall of stocks. 
+# 
+# 2.  Streamers on TWITCH:
+# 
+# - Link: https://www.kaggle.com/aayushmishra1512/twitchdata
+# - Purpose : This data consists of different things like number of viewers, number of active viewers, followers gained and many other relevant columns regarding a particular streamer.
+# 
+# 3.  IPL dataset:
+# 
+# - Link: https://www.kaggle.com/saurav9786/indian-premier-league-match-analysis
+# - Purpose :  I would use this dataset to predict the winner of the tournament.
+# 
+# 4.  UBER:
+# 
+# - Link: https://github.com/fivethirtyeight/uber-tlc-foil-response/blob/master/Uber-Jan-Feb-FOIL.csv
+# - Purpose : I will use this to find how many diapatches were made in specific day, and how trpis were taken
+# 
+# 5.  FIFA 2021:
+# 
+# - Link: https://www.kaggle.com/aayushmishra1512/fifa-2021-complete-player-data
+# - Purpose : I will use this dataset to group the age of player with his potential growth. 
